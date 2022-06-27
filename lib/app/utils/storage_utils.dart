b9a2ea7ecc0666d93d10e3d0b282b1f6802af0f5
NguyenXuanLoc <59056264+NguyenXuanLoc@users.noreply.github.com>
2022-06-27 22:40:42 +0700

    handle login, update profile.

--- lib/app/utils/storage_utils.dart
+++ lib/app/utils/storage_utils.dart
@@ -5,29 +5,37 @@ import 'package:get_storage/get_storage.dart';
 import 'package:c9p/app/config/globals.dart' as globals;
 
 class StorageUtils {
-  static void saveUser(UserModel userModel) async {
+  static Future<void> saveUser(UserModel userModel) async {
     globals.isLogin = true;
-    globals.accessToken = userModel.data?.bearerToken?.token ?? '';
+    globals.accessToken = userModel.data?.token?.token ?? '';
     await GetStorage().write(StorageKey.AccountInfo, userModel.toJson());
   }
 
-  static Future<void> getUser() async {
+  static Future<UserModel?> getUser() async {
     var userString = await GetStorage().read(StorageKey.AccountInfo);
     try {
       if (userString != null) {
         var userModel = UserModel.fromJson(userString);
         globals.isLogin = true;
-        globals.accessToken = userModel.data?.bearerToken?.token ?? '';
+        globals.accessToken = userModel.data?.token?.token ?? '';
+        return userModel;
       } else {
         globals.isLogin = false;
       }
     } catch (ex) {
       globals.isLogin = false;
     }
+    return null;
   }
 
-  static void clearUser() {
+  static Future<void> clearUser() async {
     isLogin = false;
-    GetStorage().remove(StorageKey.AccountInfo);
+    await GetStorage().remove(StorageKey.AccountInfo);
   }
+
+  static Future<void> setIsFirstOrder(bool value) async =>
+      GetStorage().write(StorageKey.firstOpenOrder, value);
+
+  static Future<bool> isFirstOrder() async =>
+      await GetStorage().read(StorageKey.firstOpenOrder) ?? true;
 }
